1d95c36c75e78cc7e3393e4f95129006628836a4
KairenZEOA <quocpham2304@gmail.com>
2023-10-10 19:57:06 +0700

    up test admin

--- src/LandingPage/LDPComponents/Menutop.js
+++ src/LandingPage/LDPComponents/Menutop.js
@@ -137,6 +137,7 @@ const Menutop = () => {
         <>
             <img style={{ height: 50, }} src="https://quocpham2304.github.io/webtuyendungmindX/img/logo-a.png" />
             <Link className="menutab" to={'/webtuyendungmindX'} style={{ color: "white", fontSize: 18 }} > Trang chủ</Link>
+            <Link className="menutab" to={'/webtuyendungmindX/admin'} style={{ color: "white", fontSize: 18 }} > admin</Link>
             <Button
                 className="btn-modal"
                 type="primary"
